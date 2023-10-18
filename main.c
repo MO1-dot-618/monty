db6cf720bc5c7f81293b5ae5da878c25f38d85df
Malika Oubilla <malikaoubilla@gmail.com>
2023-10-18 15:05:05 -0700

    0.1

--- main.c
+++ main.c
@@ -5,20 +5,33 @@ int main(int argc, char **argv)
 	char* line;
 	char* opcode;
 	FILE *file;
-	int i = 0, j = 0, line_num = 0;
+	int i = 0, j = 0, line_num = 1;
+	stack_t *stack_head;
 
 	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
+	/* initialize stack and list of opcodes */
+	stack_head = (stack_t*) malloc(sizeof(stack_t));
+	stack_head->prev = NULL;
+	stack_head->next = NULL;
+
 	line = malloc(100);
+	if (line == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 	while (fgets(line, 100, file) != NULL)
 	{
 		line_num++;
---
+++ opcodes.c
@@ -0,0 +1,8 @@
+#include "monty.h"
+
+
+
+int push(stack_t **stack, unsigned int line_number)
+{
+	j
+}
