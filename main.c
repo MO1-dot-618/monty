914273da24b0e25f5fbe5eafa8415eedd31474a9
Malika Oubilla <malikaoubilla@gmail.com>
2023-10-20 04:04:58 -0700

    stderr fprintf

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int rmonty(FILE *file)
 	line = malloc(100);
 	if (line == NULL)
 	{
-		printf("Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		return(EXIT_FAILURE);
 	}
 	while (fgets(line, 100, file) != NULL)
@@ -98,7 +98,7 @@ int rmonty(FILE *file)
 		}
 		else
 		{
-			printf("L%d: unknown instruction %s\n", line_num, opcode);
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_num, opcode);
 			free(opcode);
 			free(line);
 			free_stack(stack_head);
@@ -120,14 +120,14 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
